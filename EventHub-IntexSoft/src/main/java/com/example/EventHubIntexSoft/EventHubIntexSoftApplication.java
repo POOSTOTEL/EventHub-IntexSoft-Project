93935dab1413ca244db0d15dca12a87045eab933
POOSTOTEL <sashamix101@mail.ru>
2024-01-17 01:48:21 +0300

    Initial commit.

---
+++ EventHub-IntexSoft/src/main/java/com/example/EventHubIntexSoft/EventHubIntexSoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventHubIntexSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHubIntexSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHubIntexSoftApplication.class, args);
+	}
+
+}
---
+++ EventHub-IntexSoft/src/test/java/com/example/EventHubIntexSoft/EventHubIntexSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventHubIntexSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubIntexSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
